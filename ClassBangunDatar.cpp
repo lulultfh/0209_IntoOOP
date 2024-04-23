d3edcc1144bddc4186c0920fb6f4d1589465cb83
lulultfh <punyalulu05@gmail.com>
2024-04-23 15:49:24 +0700

    add method for procedure input in class bangunDatar

--- ClassBangunDatar.cpp
+++ ClassBangunDatar.cpp
@@ -8,4 +8,11 @@ class bangunDatar{
         float panjang, lebar;
     public:
         float Luas;
+
+        void input() { //methode input persegi panjang
+            cout << "Masukan Panjangnya =  ";
+            cin >> panjang;
+            cout << "Masukan Lebarnya = ";
+            cin >> lebar;
+        }
 };
