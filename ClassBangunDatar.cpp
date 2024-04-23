827cffe4dc2071a133f577dfd568f9048ccf52e9
lulultfh <punyalulu05@gmail.com>
2024-04-23 15:53:50 +0700

    add method for procedure output in bangunDatar's class

--- ClassBangunDatar.cpp
+++ ClassBangunDatar.cpp
@@ -19,4 +19,10 @@ class bangunDatar{
         float hitungLuas (){
             return panjang * lebar;
         }
+
+        void display() {
+            cout << "Panjangnya = " << panjang << endl;
+            cout << "Lebarnya = " << lebar << endl;
+            cout << "Luasnya = " << hitungLuas() << endl;
+        }
 };
